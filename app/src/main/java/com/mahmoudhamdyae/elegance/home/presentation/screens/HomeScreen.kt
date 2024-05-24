105fc85c5fef980a73edcd4fdfd188484c12d169
mahmoudhamdyae <mahmoudhamdyae@gmail.com>
2024-05-24 20:24:31 +0300

    Home Screen

--- app/src/main/java/com/mahmoudhamdyae/elegance/home/presentation/screens/HomeScreen.kt
+++ app/src/main/java/com/mahmoudhamdyae/elegance/home/presentation/screens/HomeScreen.kt
@@ -1,15 +1,19 @@
 package com.mahmoudhamdyae.elegance.home.presentation.screens
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.mahmoudhamdyae.elegance.core.UiState
 import com.mahmoudhamdyae.elegance.core.composables.screens.ErrorScreen
 import com.mahmoudhamdyae.elegance.core.composables.screens.LoadingScreen
 import com.mahmoudhamdyae.elegance.home.domain.models.Category
+import com.mahmoudhamdyae.elegance.home.presentation.viewmodels.HomeUiState
 import com.mahmoudhamdyae.elegance.home.presentation.viewmodels.HomeViewModel
 import org.koin.androidx.compose.getViewModel
 
@@ -18,21 +22,21 @@ fun HomeScreen(
     modifier: Modifier = Modifier,
     viewModel: HomeViewModel = getViewModel<HomeViewModel>()
 ) {
-    val uiState: UiState<List<Category>> = viewModel.uiState.collectAsState ().value.categoriesUiState
+    val uiState = viewModel.uiState.collectAsState ().value
     Box(
         modifier = modifier
     ) {
         when(
-            uiState
+            uiState.categoriesUiState
         ) {
             is UiState.Loading -> {
                 LoadingScreen()
             }
             is UiState.Success -> {
-                HomeScreenContent(uiState.data ?: emptyList())
+                HomeScreenContent(uiState)
             }
             is UiState.Error -> {
-                ErrorScreen(errorMessage = uiState.message ?: "")
+                ErrorScreen(errorMessage = uiState.categoriesUiState.message ?: "")
             }
         }
     }
@@ -40,6 +44,14 @@ fun HomeScreen(
 
 @Composable
 fun HomeScreenContent(
+    uiState: HomeUiState,
+    modifier: Modifier = Modifier
+) {
+    CategoriesList(uiState.categoriesUiState.data ?: emptyList(), modifier)
+}
+
+@Composable
+fun CategoriesList(
     categories: List<Category>,
     modifier: Modifier = Modifier
 ) {
@@ -47,7 +59,19 @@ fun HomeScreenContent(
         modifier = modifier
     ) {
         items(categories.size) { index ->
-            Text(text = categories[index].name)
+            CategoryItem(category = categories[index])
         }
+    }
+}
+
+@Composable
+fun CategoryItem(
+    category: Category,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier.padding(8.dp)
+    ) {
+        Text(text = category.name)
     }
 }
